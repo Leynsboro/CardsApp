886909c83a71e45fb207920b10927373e626cfa8
JuliDeveloper <rja-97@yandex.ru>
2022-05-24 13:49:45 +0300

    Added logic for adding a new image in the controller for adding a new book

--- CardsApp/ViewControllers/NewBookTableViewController.swift
+++ CardsApp/ViewControllers/NewBookTableViewController.swift
@@ -18,7 +18,7 @@ class NewBookTableViewController: UITableViewController {
     @IBOutlet var saveButton: UIBarButtonItem!
     
     var book = Book(name: "", author: "", description: "", image: UIImage(named: ""), isFavorite: false)
-    var imageChance = false
+    var imageChange = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,10 +36,17 @@ class NewBookTableViewController: UITableViewController {
         let name = newNameTextField.text ?? ""
         let author = newAuthorTextField.text ?? ""
         let description = newDescriptionTextField.text ?? ""
-        var image = newBookImage.image
-        image = UIImage(named: "image")
+        let image = imageChange ? newBookImage.image : UIImage(named: "image")
         
         book = Book(name: name, author: author, description: description, image: image, isFavorite: book.isFavorite)
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            createAlertSheet()
+        } else {
+            view.endEditing(true)
+        }
     }
 }
 
@@ -51,6 +58,52 @@ extension NewBookTableViewController {
         let description = newDescriptionTextField.text ?? ""
         
         saveButton.isEnabled = !name.isEmpty && !author.isEmpty && !description.isEmpty
+    }
+    
+    private func createAlertSheet() {
+        let camera = UIImage(systemName: "camera")
+        let photo = UIImage(systemName: "photo")
+        
+        let alertSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { _ in
+            self.choseImagePicker(source: .camera)
+        }
+        cameraAction.setValue(camera, forKey: "image")
+        
+        let photoAction = UIAlertAction(title: "Photo", style: .default) { _ in
+            self.choseImagePicker(source: .photoLibrary)
+        }
+        photoAction.setValue(photo, forKey: "image")
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertSheet.addAction(cameraAction)
+        alertSheet.addAction(photoAction)
+        alertSheet.addAction(cancelAction)
+        
+        present(alertSheet, animated: true)
+    }
+}
+
+extension NewBookTableViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    private func choseImagePicker(source: UIImagePickerController.SourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        newBookImage.image = info[.editedImage] as? UIImage
+        newBookImage.contentMode = .scaleAspectFill
+        newBookImage.clipsToBounds = true
+        
+        imageChange = true
+        dismiss(animated: true)
     }
 }
 
